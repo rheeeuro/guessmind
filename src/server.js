d1a271488bbeb9a7b885e6623df2b09a683fae70
eurohand <eurohand@naver.com>
2019-09-21 20:32:53 +0900

    Gulp part

--- src/server.js
+++ src/server.js
@@ -12,12 +12,20 @@ app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
-  console.log(`✅ Server running: http://localhost:${PORT}`);
+  console.log(`☞ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
 io.on("connection", socket => {
-  socket.on("helloGuys", () => console.log("the client said hello"));
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon"
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
