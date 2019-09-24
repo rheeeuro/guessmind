0314fde73177442bd2d10639468f4674199cb19d
eurohand <eurohand@naver.com>
2019-09-25 00:39:01 +0900

    Realtime Painting

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -5,12 +5,12 @@ const sendMsg = document.getElementById("jsSendMsg");
 
 const appendMsg = (text, nickname) => {
   const li = document.createElement("li");
-  li.innerHTML = `
-    <span class="author ${nickname ? "out" : "self"}">${
+  li.innerHTML = `<span class="author ${nickname ? "out" : "self"}">${
     nickname ? nickname : "나"
-  }: </span> ${text}
-  `;
+  }: </span> ${text}`;
   messages.appendChild(li);
+  const position = messages.scrollHeight;
+  messages.scrollTop = position;
 };
 
 const handleSendMsg = event => {
--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,15 +1,18 @@
 import { handleNewUser, handleDisconnected } from "./notifications";
 import { handleNewMessage } from "./chat";
+import { handleBeganPath, handleStrokedPath, handleFilled } from "./paint";
 
 let socket = null;
 
 export const getSocket = () => socket;
-export const updateSocket = newSocket => (socket = newSocket);
 
 export const initSockets = newSocket => {
   const { events } = window;
-  updateSocket(newSocket);
-  newSocket.on(events.newUser, handleNewUser);
-  newSocket.on(events.disconnected, handleDisconnected);
-  newSocket.on(events.newMsg, handleNewMessage);
+  socket = newSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handleDisconnected);
+  socket.on(events.newMsg, handleNewMessage);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
 };
